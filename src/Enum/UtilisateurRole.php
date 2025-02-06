ae77755168bd5f7d6e21e0133686ff05a2df839b
haythemdr <haythemdridi633@gmail.com>
2025-02-06 13:26:07 +0100

    haythem

---
+++ src/Enum/UtilisateurRole.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+enum UtilisateurRole: string
+{
+    case Patient = 'Patient';
+    case Medecin = 'Medecin';
+    case Infirmier = 'Infirmier';
+    case Pharmacien = 'Pharmacien';
+    case Responsable = 'Responsable';
+    case Administrateur = 'Administrateur';
+    case FemmeDeMenage = 'Femme de menage';
+}
